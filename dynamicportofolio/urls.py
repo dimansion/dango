61c1131fbf1d74cce80380c421ae108a31aa195c
dimansion <dimansional@gmail.com>
2015-12-13 21:48:42 +0700

    dynamic blog kurang styling

--- dynamicportofolio/urls.py
+++ dynamicportofolio/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.portofolio_list, name='portofolio_list'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

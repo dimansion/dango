d53f5df898e2ee956ee28453dc3d77c856c9c97b
dimansion <dimansional@gmail.com>
2015-12-15 00:42:09 +0700

    belum betul

--- portofolio/urls.py
+++ portofolio/urls.py
@@ -1,6 +1,11 @@
-from django.conf.urls import patterns, url
-from portofolio import views
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),)
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^$', views.portofolio_list, name='portofolio_list')
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 

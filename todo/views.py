8d02bcd7a1840954632fbe2c49809417dbb32697
dimansion <dimansional@gmail.com>
2016-09-02 20:00:00 +0700

    modified todo

--- todo/views.py
+++ todo/views.py
@@ -6,20 +6,17 @@ from .forms import ScheduleForm
 
 def post_list(request):
     posts = Schedule.objects.all()
-    return render(request, 'todo/list.html', {'posts': posts})
+    if request.method == "POST":
+        form = ScheduleForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('post_list')
+    else:
+        form = ScheduleForm()
+    return render(request, 'todo/list.html', {'posts': posts, 'form': form})
 
 def post_remove(request, pk):
     post = get_object_or_404(Schedule, pk=pk)
     post.delete()
     return redirect('todo.views.post_list')
-
-def post_new(request):
-    if request.method == 'POST':
-        if form.is_valid():
-	    form.save(commit=True)
-            return post_list(request)
-	else:
-            print form.errors
-    else:
-        form = ScheduleForm()
-    return render(request, 'todo/list.html', {'form': form})
